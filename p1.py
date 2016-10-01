73e3892bf48892c6e0aeeda90e09966a5242ad7a
urlee <urlee@ucsc.edu>
2016-10-01 15:54:13 -0700

    Q is a heap

--- p1.py
+++ p1.py
@@ -4,8 +4,6 @@ from heapq import heappop, heappush
 
 
 def dijkstras_shortest_path(initial_position, destination, graph, adj):
-    # Returns a list containing all cells from initial_position
-    # to destination.
     """ Searches for a minimal cost path through a graph using Dijkstra's algorithm.
 
     Args:
@@ -20,31 +18,27 @@ def dijkstras_shortest_path(initial_position, destination, graph, adj):
 
     """
     """
-    Skip the entire initialization loop, 
+    Skip the entire initialization loop,
     instead use dicts:
     dist[state] = better_distance
     prev[state2] = state1
     """
+    # Return prev as a list
     Q = []  # Create a vertex set Q, (list)
-    dist = {}  # Distance
-    dist['source'] = 0  # Distance from source to source
-    prev = {}  # "Back Pointer"
-    for v in graph:
-        if v is not 'source':  # Literally the key/string, 'source'
-            dist[v] = math.inf  # Unknown distance from source to v
-            prev[v] = None  # Set to Undefined
-        Q[v] = dist[v]  # Pseudo says to Q.add_with_priority(v, dist[v])
-        # This may or may not satisfy that
+    heappush(Q, (starting_position, 0)
+    dist={}  # Distance
+    prev={}  # "Back Pointer"
     while Q:
-        u = min(Q, key=Q.get)  # Remove & return best vertex
+        u=min(Q, key=Q.get)  # Remove & return best vertex
+        u=Q.nsmallest(1, Q
         for v in Q:  # Everything else besides best vertex
             if not best_vertex:
-                alt = dist[u]  # + dist(u, v)
+                alt=dist[u]  # + dist(u, v)
             if alt < dist[v]:
-                dist[v] = alt
-                prev[v] = u
+                dist[v]=alt
+                prev[v]=u
                 Q.decrease_priority(v, alt)
-    """ 
+    """
     Because dist will not be defined for unvisited states,
     the expression "alt < dist[v]" must be implemented as
     "v not in dist or alt < dist[v]" or
@@ -58,7 +52,7 @@ def dijkstras_shortest_path(initial_position, destination, graph, adj):
     """
     """
     Instead of returning the "dist" and "prev" tables (dicts),
-    recover a specific shortest path and return it instead. 
+    recover a specific shortest path and return it instead.
     Represent it as a list of states that starts with their
     source state and ends with the destination state.
     """
@@ -96,17 +90,17 @@ def navigation_edges(level, cell):
              ((1,1), 1.4142135623730951),
              ... ]
     """
-    adjacencies = []
-    x, y = cell
+    adjacencies=[]
+    x, y=cell
     for dx in [-1, 0, 1]:
         for dy in [-1, 0, 1]:
             # Makes sure to check if the adjacent squres
             if dx != 0 or dy != 0 and (x + dx, y + dy) in level['spaces']:
                 if dx == 0 or dy == 0:
-                    new_dist = (
+                    new_dist=(
                         0.5 * level['spaces'][x][y] + 0.5 * level['spaces'][x + dx][y + dy])
                 else:
-                    new_dist = (
+                    new_dist=(
                         sqrt(2) * (level['spaces'][x][y] + level['spaces'][x + dx][y + dy]))
                 adjacencies.append(((x + dx, y + dy), new_dist))
 
@@ -125,15 +119,15 @@ def test_route(filename, src_waypoint, dst_waypoint):
     """
 
     # Load and display the level.
-    level = load_level(filename)
+    level=load_level(filename)
     show_level(level)
 
     # Retrieve the source and destination coordinates from the level.
-    src = level['waypoints'][src_waypoint]
-    dst = level['waypoints'][dst_waypoint]
+    src=level['waypoints'][src_waypoint]
+    dst=level['waypoints'][dst_waypoint]
 
     # Search for and display the path from src to dst.
-    path = dijkstras_shortest_path(src, dst, level, navigation_edges)
+    path=dijkstras_shortest_path(src, dst, level, navigation_edges)
     if path:
         show_level(level, path)
     else:
@@ -141,7 +135,7 @@ def test_route(filename, src_waypoint, dst_waypoint):
 
 
 def cost_to_all_cells(filename, src_waypoint, output_filename):
-    """ Loads a level, calculates the cost to all reachable cells from 
+    """ Loads a level, calculates the cost to all reachable cells from
     src_waypoint, then saves the result in a csv file with name output_filename.
 
     Args:
@@ -152,21 +146,21 @@ def cost_to_all_cells(filename, src_waypoint, output_filename):
     """
 
     # Load and display the level.
-    level = load_level(filename)
+    level=load_level(filename)
     show_level(level)
 
     # Retrieve the source coordinates from the level.
-    src = level['waypoints'][src_waypoint]
+    src=level['waypoints'][src_waypoint]
 
     # Calculate the cost to all reachable cells from src and save to a csv
     # file.
-    costs_to_all_cells = dijkstras_shortest_path_to_all(
+    costs_to_all_cells=dijkstras_shortest_path_to_all(
         src, level, navigation_edges)
     save_level_costs(level, costs_to_all_cells, output_filename)
 
 
 if __name__ == '__main__':
-    filename, src_waypoint, dst_waypoint = 'example.txt', 'a', 'e'
+    filename, src_waypoint, dst_waypoint='example.txt', 'a', 'e'
 
     # Use this function call to find the route between two waypoints.
     test_route(filename, src_waypoint, dst_waypoint)
